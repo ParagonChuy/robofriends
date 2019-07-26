7d2761cc8b05ab4987445751dc9a0e8d97ce901b
jesus vega <jesusvega@jesuss-MBP.lan>
2019-07-26 14:07:10 -0700

    Initial commit from Create React App

---
+++ src/robots.js
@@ -0,0 +1,9 @@
+export const robots = [
+  {id: 1, name: 'Leanne Graham', username: 'lean', email: 'lean@ne.com'},
+  {id: 2, name: 'John Red', username: 'rjohn', email: 'rjohn@john.com'},
+  {id: 3, name: 'Jeff Gold', username: 'golf', email: 'golf@ewne.com'},
+  {id: 4, name: 'Estefi Graham', username: 'estefierrote', email: 'estefierrote@gmail.com'},
+  {id: 5, name: 'Dos Caguamas', username: '4loko', email: '4loko@loko.com'},
+  {id: 6, name: 'Rayos Boz', username: 'boz', email: 'boz@lightyee.com'},
+  {id: 7, name: 'Me Hawmie', username: 'comebackallthetime', email: 'foo@gonewild.com'},
+]
